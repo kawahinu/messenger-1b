7f58d4d855a192aabda494685cc415c381a1b155
kawahinu <70953424+kawahinu@users.noreply.github.com>
2021-08-03 01:15:38 +0900

    ngaa
    CLI苦手だから別のサーバを検討するかも

---
+++ messenger/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+  </body>
+  <!-- The core Firebase JS SDK is always required and must be listed first -->
+  <script src="/__/firebase/8.8.1/firebase-app.js"></script>
+
+  <!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+  <script src="/__/firebase/8.8.1/firebase-analytics.js"></script>
+
+  <!-- Initialize Firebase -->
+  <script src="/__/firebase/init.js"></script>
+</html>
